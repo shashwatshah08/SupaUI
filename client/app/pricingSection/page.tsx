9001a5a70efda8ffc20f7090c32ce0d235d3de99
Shashwat Shah <shashwatshah08@yahoo.com>
2024-05-23 00:21:53 -0300

    fixed card height issue

--- client/app/pricingSection/page.tsx
+++ client/app/pricingSection/page.tsx
@@ -4,7 +4,7 @@ import PricingSection1, { pricingSection1CodeString } from "./pricingSection1";
 export default function pricingSection() {
   return (
     <div className="flex flex-col space-y-10">
-      <div className="h-[80vh] overflow-auto">
+      <div className="h-[85vh] overflow-auto">
         <ResizableComponentPanel
           component={PricingSection1}
           codeString={pricingSection1CodeString}
