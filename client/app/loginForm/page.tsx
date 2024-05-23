5ec3a7eb9da273ce1671bcb1061d46004c9b065a
Shashwat Shah <shashwatshah08@yahoo.com>
2024-05-22 22:01:15 -0300

    login form 2 built

--- client/app/loginForm/page.tsx
+++ client/app/loginForm/page.tsx
@@ -5,8 +5,15 @@ import {
 } from "@/components/ui/resizable";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import LoginForm1, { loginForm1CodeString } from "./loginForm1";
+import LoginForm2, { loginForm2CodeString } from "./loginForm2";
 
-export default function LoginForm() {
+function ResizableComponentPanel({
+  component: Component,
+  codeString,
+}: {
+  component: any;
+  codeString: string;
+}) {
   return (
     <ResizablePanelGroup
       className="max-w-full w-full h-[calc(100vh-theme(spacing.16))] p-4 rounded-lg border dark:border-gray-800"
@@ -15,9 +22,9 @@ export default function LoginForm() {
       <ResizablePanel defaultSize={100}>
         <div className="h-full flex flex-col gap-4 overflow-y-auto p-4">
           <div className="grid gap-2">
-            <h2 className="text-2xl font-bold">Login Form 1</h2>
+            <h2 className="text-2xl font-bold">Component</h2>
           </div>
-          <LoginForm1 />
+          <Component />
         </div>
       </ResizablePanel>
       <ResizableHandle withHandle />
@@ -29,11 +36,28 @@ export default function LoginForm() {
               View the TSX code for the selected component.
             </p>
           </div>
-          <SyntaxHighlighter language="tsx">
-            {loginForm1CodeString}
-          </SyntaxHighlighter>
+          <SyntaxHighlighter language="tsx">{codeString}</SyntaxHighlighter>
         </div>
       </ResizablePanel>
     </ResizablePanelGroup>
+  );
+}
+
+export default function LoginForm() {
+  return (
+    <div className="flex flex-col space-y-10">
+      <div className="h-screen overflow-auto">
+        <ResizableComponentPanel
+          component={LoginForm1}
+          codeString={loginForm1CodeString}
+        />
+      </div>
+      <div className="h-screen overflow-auto">
+        <ResizableComponentPanel
+          component={LoginForm2}
+          codeString={loginForm2CodeString}
+        />
+      </div>
+    </div>
   );
 }
